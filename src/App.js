ba720ad0665d841f83dfaa826b0792a23c3b7d3b
Akif Emre Senol <akifffemreee@gmail.com>
2023-06-15 17:43:09 +0300

    created navbar and add assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
