291d24a669e1355930675f93ce3309784fdd4873
Akif Emre Senol <akifffemreee@gmail.com>
2023-07-14 13:32:45 +0300

    add screenshots for readme

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
       className="w-full md:h-screen text-gray-300 bg-[#221b42]"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+        <div className="pb-8 ">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-500">
             Projects
           </p>
